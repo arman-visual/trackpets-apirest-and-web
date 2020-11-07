5f92db97c2db3b805d92023198bc432c29180edd
Arman <arman.agora@gmail.com>
2020-11-07 16:29:38 +0100

    Add Pojos faltantes y Vistas basicas
    Se ha añadido los POJOS restantes(Animal, Protectora, y Personas) con atributos y relaciones ya definidas por el equipo. Tambien se han añadido las vistas(html) para ser invocados en caso de querer añadir mediante formularios cualquiera de los objetos ya mencionados.

--- src/main/java/com/trackpets/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/trackpets/springboot/web/app/controller/IndexController.java
@@ -3,17 +3,36 @@ package com.trackpets.springboot.web.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class IndexController {
 	/**
 	 * Nos lleva al index de nuestra web
 	 * 
 	 * @return vista html indicada
 	 */
-	@GetMapping(value = {"/","/index","/home"})
-	public String index(ModelMap model) {
-		model.addAttribute("titulo", "Track Pets Web Oficial");
+	@GetMapping(value = "/")
+	public String index(ModelMap modelmap) {
+		modelmap.addAttribute("titulo", "Track Pets Web Oficial");
 		return "index";
+	}
+	
+	@GetMapping(value = "/addAnimal")
+	public String addAnimal(ModelMap modelmap) {
+		modelmap.addAttribute("titulo", "Alta animal");
+		return "formPet";
+	}
+	
+	@GetMapping(value = "/addProtectora")
+	public String addProtectora(ModelMap modelmap) {
+		modelmap.addAttribute("titulo", "Alta protectora");
+		return "formProtectora";
+	}
+	@GetMapping(value = "/addPersona")
+	public String addPersona(ModelMap modelmap) {
+		modelmap.addAttribute("titulo", "Alta protectora");
+		return "formPersona";
 	}
 }
--- src/main/java/com/trackpets/springboot/web/app/models/entity/Animal.java
+++ src/main/java/com/trackpets/springboot/web/app/models/entity/Animal.java
@@ -1,31 +1,61 @@
 package com.trackpets.springboot.web.app.models.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "animales")
 public class Animal implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_animal")
 	private Long id;
-	
-	@Column(name="nombre")
-	private String nombre;
-	
-	@Column(name="raza")
+
+	@Column(name = "dni_animal", length = 20, nullable = true)
+	private String dni;
+
+	@Column(name = "raza", length = 30, nullable = false)
 	private String raza;
+
+	@Column(name = "edad", length = 2, nullable = true)
+	private int edad;
+
+	@Column(name = "tamaño", length = 30, nullable = true)
+	private String tamaño;
+
+	@Column(name = "nombre", length = 30, nullable = false)
+	private String nombre;
+
+	@Column(name = "fecha_de_adopcion", nullable = true)
+	@Temporal(TemporalType.DATE)
+	private Date fechaAdopcion;
+
+	@Column(name = "fecha_de_alta", nullable = true)
+	@Temporal(TemporalType.DATE)
+	private Date fechaAlta;
+
+	@Column(name = "descripcion", length = 250, nullable = true)
+	private String descripcion;
+
+	@Column(name = "disponible", nullable = false)
+	private Boolean estado;
+
+	@JoinColumn(name = "id_protectora")
+	@OneToOne(fetch = FetchType.LAZY)
+	private Protectora herramienta;
 
 	public Long getId() {
 		return id;
@@ -35,12 +65,12 @@ public class Animal implements Serializable {
 		this.id = id;
 	}
 
-	public String getNombre() {
-		return nombre;
+	public String getDni() {
+		return dni;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	public void setDni(String dni) {
+		this.dni = dni;
 	}
 
 	public String getRaza() {
@@ -51,4 +81,76 @@ public class Animal implements Serializable {
 		this.raza = raza;
 	}
 
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getTamaño() {
+		return tamaño;
+	}
+
+	public void setTamaño(String tamaño) {
+		this.tamaño = tamaño;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaAdopcion() {
+		return fechaAdopcion;
+	}
+
+	public void setFechaAdopcion(Date fechaAdopcion) {
+		this.fechaAdopcion = fechaAdopcion;
+	}
+
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+	public Protectora getHerramienta() {
+		return herramienta;
+	}
+
+	public void setHerramienta(Protectora herramienta) {
+		this.herramienta = herramienta;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 }
