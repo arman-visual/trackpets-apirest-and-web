3d2318e6470e6f8b45b78b6e495174a30d2c8ff8
Carlos-Rivas <trackpets2020@gmail.com>
2020-12-08 11:26:27 +0100

    correo

--- src/main/java/com/trackpets/springboot/web/app/configuration/WebMvcConfiguration.java
+++ src/main/java/com/trackpets/springboot/web/app/configuration/WebMvcConfiguration.java
@@ -27,8 +27,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 	    mailSender.setHost("smtp.gmail.com");
 	    mailSender.setPort(587);
 	    mailSender.setProtocol("smtp");
-	    mailSender.setUsername(".....@gmail.com");
-	    mailSender.setPassword(".....");
+	    mailSender.setUsername("trackpets2020@gmail.com");
+	    mailSender.setPassword("@trackpets2020");
 	    
 	    Properties props = mailSender.getJavaMailProperties();
 	    props.put("mail.transport.protocol", "smtp");
