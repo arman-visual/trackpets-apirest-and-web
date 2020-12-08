b08fea55219cff10d51269ae6232f2f4cc10d084
Arman <arman.agora@gmail.com>
2020-12-08 10:53:22 +0100

    Dando estilo a las Cards de Buscar.html
    Se han dado estilo a las cards de buscar.html y modificado el campo "tamaño" a "tamanyo" por problemas con el codigo usando "ñ"

--- src/main/java/com/trackpets/springboot/web/app/models/entity/Mascota.java
+++ src/main/java/com/trackpets/springboot/web/app/models/entity/Mascota.java
@@ -36,8 +36,8 @@ public class Mascota implements Serializable {
 	@Column(name = "edad", length = 2, nullable = true)
 	private int edad;
 
-	@Column(name = "tamaño", length = 30, nullable = true)
-	private String tamaño;
+	@Column(name = "tamanyo", length = 30, nullable = true)
+	private String tamanyo;
 
 	@Column(name = "nombre", length = 30, nullable = false)
 	private String nombre;
@@ -100,12 +100,12 @@ public class Mascota implements Serializable {
 		this.edad = edad;
 	}
 
-	public String getTamaño() {
-		return tamaño;
+	public String getTamanyo() {
+		return tamanyo;
 	}
 
-	public void setTamaño(String tamaño) {
-		this.tamaño = tamaño;
+	public void setTamaño(String tamanyo) {
+		this.tamanyo = tamanyo;
 	}
 
 	public String getNombre() {
